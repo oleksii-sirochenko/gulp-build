3bd970107f4e17541a663e4c62038d063b802b31
alex-2077 <sirochenko.aleksey@gmail.com>
2020-09-20 12:51:53 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# gulp-typescript-build
+# Gulp Typescript build
+
+Version 1.0.0
+
+This repository will be useful if you are going to build a small-medium tier project and you want to write your frontend in Typescript with SCSS styling.
+
+Gulp config fits very good with Wordpress project. You don't have to deal with Webpack and you can start development quickly.
+
+####1. Open terminal.
+
+####2. We need to start a project, step into desired directory and execute:
+
+npm init
+
+####3.Install gulp-cli if you don't have one:
+
+npm install -g gulp-cli
+
+####4. Install required packages (one line):
+
+npm install --save-dev gulp-sass gulp-autoprefixer gulp-clean-css gulp-plumber gulp-notify gulp-rename del gulp-filter typescript gulp@4.0.0 browserify tsify watchify vinyl-source-stream gulp-uglify vinyl-buffer gulp-sourcemaps babelify@8 babel-core babel-preset-env gulp-append-prepend eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin
+
+
+####5. Configure gulpfile.js. You can change paths to files, folders. Add different TS files to separate compiling.
+
+####6. Use available Gulp tasks to build your frontend project.
+```
+/**
+ * --------------------
+ * Available gulp tasks
+ * --------------------
+ * Project build:
+ *
+ * gulp build
+ * --------------------
+ * SCSS:
+ *
+ * gulp build-scss
+ * gulp watch-scss
+ * --------------------
+ * Typescript:
+ *
+ * gulp build-ts
+ * gulp watch-ts
+ * --------------------
+ */
+ ```
+
+#####Useful links:
+https://www.typescriptlang.org/docs/handbook/gulp.html
+
+Copyright (c) Aleksey Sirochenko https://github.com/alex-2077/
+
+MIT License https://opensource.org/licenses/MIT
---
+++ assets/gulpfile.js
@@ -0,0 +1,179 @@
+const gulp = require('gulp');
+
+// utilities
+const rename = require('gulp-rename');
+const del = require('del');
+const plumber = require('gulp-plumber');
+const filter = require('gulp-filter');
+const notify = require('gulp-notify');
+
+// scss,css
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+
+// ts
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const tsify = require('tsify');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const buffer = require('vinyl-buffer');
+const watchify = require('watchify');
+
+/**
+ * --------------------
+ * Available gulp tasks
+ * --------------------
+ * Project build:
+ *
+ * gulp build
+ * --------------------
+ * SCSS:
+ *
+ * gulp build-scss
+ * gulp watch-scss
+ * --------------------
+ * Typescript:
+ *
+ * gulp build-ts
+ * gulp watch-ts
+ * --------------------
+ */
+
+/**
+ * Builds entire frontend project
+ */
+gulp.task('build', async () => {
+    // empty dist folder
+    del.sync('dist/**');
+    
+    // move files that not required compiling from src to dist
+    gulp.src('src/css/*.css').pipe(gulp.dest('dist/css'));
+    gulp.src('src/css/libs/*.css').pipe(gulp.dest('dist/css/libs'));
+    gulp.src('src/fonts/**/*').pipe(gulp.dest('dist/fonts'));
+    gulp.src('src/js/*.js').pipe(gulp.dest('dist/js'));
+    gulp.src('src/js/libs/*.js').pipe(gulp.dest('dist/js/libs'));
+    gulp.src('src/img/**/*').pipe(gulp.dest('dist/img'));
+    
+    // build dist files
+    gulp.series('build-ts', 'build-scss')();
+});
+
+/**
+ * Processes SCSS files to CSS, creates sourcemap for CSS, creates minified file of CSS.
+ */
+gulp.task('build-scss', async () => {
+    const list = [
+        'src/scss/style.scss',
+        'src/scss/admin-style.scss',
+    ];
+    
+    for (let i = 0; i < list.length; i++) {
+        gulp.src(list[i])
+            .pipe(plumber({
+                errorHandler: (err) => {
+                    notify.onError({
+                        title: "Gulp",
+                        subtitle: "Failure!",
+                        message: "Error: <%= error.message %>",
+                        sound: "Basso"
+                    })(err);
+                    this.emit('end');
+                }
+            }))
+            .pipe(sourcemaps.init())
+            .pipe(sass())
+            .pipe(autoprefixer(['last 10 versions'], {cascade: true}))
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest('dist/css'))
+            .pipe(filter('**/!(*.map)*.css'))
+            .pipe(cleanCSS())
+            .pipe(rename({suffix: '.min'}))
+            .pipe(gulp.dest('dist/css'));
+    }
+});
+
+/**
+ * Watches SCSS folder for changes and invokes 'build-scss' task to process SCSS files.
+ */
+gulp.task('watch-scss', () => {
+    gulp.series('build-scss')();
+    gulp.watch('src/scss/**/*.scss', gulp.series('build-scss'));
+});
+
+gulp.task('build-ts', scriptsInit.bind(null, false));
+gulp.task('watch-ts', scriptsInit.bind(null, true));
+
+/**
+ * Builds typescript files. Add your settings for each typescript file that should be built separately.
+ */
+async function scriptsInit(watch) {
+    const list = [
+        {
+            pkg: getBrowserify('src/ts/page1/page1.ts'),
+            filename: 'page1.js',
+            dist: 'dist/js/page1',
+        },
+        {
+            pkg: getBrowserify('src/ts/page2/page2.ts'),
+            filename: 'page2.js',
+            dist: 'dist/js/page2',
+        },
+    ];
+    processScripts(list, watch);
+}
+
+function getBrowserify(entries) {
+    return browserify({
+        basedir: '.',
+        ignoreWatch: ['**\/node_modules\/**'],
+        debug: true,
+        entries: entries,
+        cache: {},
+        packageCache: {}
+    })
+        .plugin(tsify)
+        .transform('babelify', {
+            presets: ['env'],
+            extensions: ['.ts']
+        });
+}
+
+function bundle() {
+    return this.pkg.bundle()
+        .on('error', (error) => {
+            console.error(error.toString());
+        })
+        .pipe(source(this.filename))
+        .pipe(buffer())
+        .pipe(sourcemaps.init())
+        .pipe(gulp.dest(this.dist))
+        .pipe(sourcemaps.write('./'))
+        .pipe(filter('**/!(*.map)*.js'))
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(this.dist));
+}
+
+function processScripts(list, watch) {
+    const tasks = [];
+    for (let i = 0; i < list.length; i++) {
+        const task = `build-ts: filename - ${list[i].filename}`;
+        const preparedBundle = bundle.bind(list[i]);
+        if (watch) {
+            watchify(list[i].pkg)
+                .on('update', preparedBundle)
+                .on("time", (timeMs) => {
+                    const date = new Date();
+                    pad = (val) => val < 10 ? '0' + val : val;
+                    console.log(`[${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}] Finished: ${list[i].filename} ${timeMs}ms`);
+                });
+            ;
+        }
+        gulp.task(task, preparedBundle);
+        tasks.push(task);
+    }
+    gulp.parallel(tasks)();
+}
+
